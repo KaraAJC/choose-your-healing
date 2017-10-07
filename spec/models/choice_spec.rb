a55432de020724bd4d4e43fc87ca06e28c8c3213
karaAJC <KaraAJC@gmail.com>
2017-10-07 12:52:24 -0500

    adds tests for Choice

---
+++ spec/models/choice_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe Choice, :type => :model do
+  let(:yoga) { Choice.create!(choice: 'yoga', cost: 2) }
+  it 'has a title' do
+    expect(yoga.title).to eq('yoga')
+  end
+
+  it 'has a cost' do
+    expect(yoga.cost).to eq(2)
+  end
+  context 'a cost is not given' do
+    let(:hugs) { Choice.create!(title: 'hugs') }
+    it 'defaults to free, unless a cost is given' do
+      expect(hugs.cost).to eq(0)
+    end
+  end
+end
